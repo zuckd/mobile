f65fb62f79ba59e7eaf72e8d7c309893a515b8d0
Ishraque99 <izahin@live.com>
2020-10-18 00:53:26 +1000

    fixed accidental comment

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 // Optionally import the services that you want to use
-// import "firebase/auth";
-//import "firebase/database";
-// import "firebase/firestore";
-// import "firebase/functions";
-// import "firebase/storage";
+import "firebase/auth";
+// import "firebase/database";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/storage";
 
 // Initialize Firebase
 const firebaseConfig = {
